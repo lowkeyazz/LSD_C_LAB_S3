541715eb2d8638a02bff09f8b8578b5f25e357ae
thealexjr <80365738+thealexjr@users.noreply.github.com>
2021-11-20 02:05:25 +0100

    Create header.h

---
+++ TP02/EX1/header.h
@@ -0,0 +1,13 @@
+#ifndef HEADER
+#define HEADER
+
+#include <stdio.h>
+#include <stdlib.h>
+
+float **sum_m(float **, float **, int );
+float **mult_m(float **, float **, int );
+float **trans_m(float **, int );
+float **dia_m(float **, int );
+
+
+#endif
